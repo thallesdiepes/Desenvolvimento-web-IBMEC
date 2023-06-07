e1a1f318a483474fd3edd142e87749f53b6bc788
thallesdiepes <86641760+thallesdiepes@users.noreply.github.com>
2023-06-06 23:13:33 -0300

    Create dados.js

---
+++ Semana-09/dados.js
@@ -0,0 +1,19 @@
+const meus_objetos = [
+    {
+        nome: 'um',
+        valor: 1,
+        descri: 'Imagem do número com fundo vermelho.'
+    },
+    {
+        nome: 'dois',
+        valor: 2,
+        descri: 'Imagem do número 2 versão com uma visão em diagonal.'
+    },
+    {
+        nome: 'três',
+        valor: 3,
+        descri: 'Imagem do número 3 desenhada com linha preta.'
+    },
+    
+    
+];
